23574609f4e682281a154dae8975e62f81938106
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-11-02 23:36:26 +0530

    added fibonaccinumber

---
+++ Easy/fibonacci-number.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/fibonacci-number/
+class Solution:
+    def fib(self, N: int) -> int:
+        if N <= 1:
+            return N
+        return self.fib(N-1) + self.fib(N-2)
