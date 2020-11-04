45448f8ce470f1360c00fcca4441ce3b4a705ce0
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-11-04 13:41:40 +0530

    added matrix-diagonal-sum o(N) Solution also

--- Easy/matrix-diagonal-sum.py
+++ Easy/matrix-diagonal-sum.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/matrix-diagonal-sum/
 
+"""
+Approach One Works on O(n^2) Solution
+"""
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
         matSum = 0
@@ -14,6 +17,28 @@ class Solution:
                 if i + j == length - 1:
                     secondary += mat[i][j]
         
+        matSum = primary + secondary
+
+        if length % 2 != 0:
+            Number_of_Elements = length * length
+            Row_Col = length + length
+            idx = Number_of_Elements // Row_Col
+            matSum -= mat[idx][idx]
+        return matSum
+
+"""
+Approach Two Works on O(n) Solution
+"""
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        matSum = 0
+        length = len(mat)
+        primary, secondary = 0, 0
+
+        for i in range(0, length):
+            primary = mat[i][i]
+            secondary = mat[i][length-i-1]
+
         matSum = primary + secondary
 
         if length % 2 != 0:
